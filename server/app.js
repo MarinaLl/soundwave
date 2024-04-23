e481b07adc4dae55b7b30905b465f58cc1bc83c8
MarinaLl <llambrichdev@gmail.com>
2024-04-23 13:56:03 +0200

    set db

--- server/app.js
+++ server/app.js
@@ -3,11 +3,24 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const cors = require('cors');
 
 var app = express();
+app.use(cors());
+
+// Conexión a MongoDB
+mongoose.connect('mongodb://localhost:27017/soundwave', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log("Conexión exitosa a MongoDB");
+}).catch(err => {
+  console.error("Error al conectar a MongoDB:", err);
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
